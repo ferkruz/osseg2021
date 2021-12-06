9386dc133f86b62cb981bc32f0cb359b784acb56
ferkruz <fercruzhigel@hotmail.com>
2021-12-05 21:40:19 -0300

    2140

--- src/Components/CartAfi/Index.js
+++
@@ -1,6 +0,0 @@
-export { default as OrderConfirmation } from "./OrderConfirmation";
-
-export { default as ProvCartilla } from "./ProvCartilla";
-export { default as LocCartilla } from "./LocCartilla";
-export { default as ModCartilla } from "./ModCartilla";
-export { default as ReviewOrder } from "./ReviewOrder";
